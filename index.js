c120caa8c13eb83062ea27adb237654f7ec86cd9
Igor Kowalczyk <xxmajonezxx@gmail.com>
2020-09-07 12:26:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ var nodemailer = require('nodemailer');
 // setup the server
 // listen on port specified by the `PORT` env var
 var server = http.createServer(function (req, res) {
-    if (req.method.toLowerCase() == 'get') {
-        displayForm(res);
-    } else if (req.method.toLowerCase() == 'post') {
-        //processAllFieldsOfTheForm(req, res);
-        processFormFieldsIndividual(req, res);
-    }
+ if (req.method.toLowerCase() == 'get') {
+  displayForm(res);
+ } else if (req.method.toLowerCase() == 'post') {
+  processAllFieldsOfTheForm(req, res);
+  //processFormFieldsIndividual(req, res);
+ }
 });
+
 var port = process.env.PORT || 8080;
 server.listen(port);
 console.log("Server runs on port", port);
@@ -22,59 +23,55 @@ console.log("Server runs on port", port);
 // serve HTML file
 // located according to the `FORM` env var
 function displayForm(res) {
-    fs.readFile(process.env.FORM || 'form.html', function (err, data) {
-        res.writeHead(200, {
-            'Content-Type': 'text/html',
-                'Content-Length': data.length
-        });
-        res.write(data);
-        res.end();
-    });
+ fs.readFile(process.env.FORM || 'form.html', function (err, data) {
+  res.writeHead(200, {
+   'Content-Type': 'text/html',
+   'Content-Length': data.length
+  });
+  res.write(data);
+  res.end();
+ });
 }
 
 // get the POST data 
 // and call the sendMail method
 function processAllFieldsOfTheForm(req, res) {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-        //Store the data from the fields in your data store.
-        //The data store could be a file or database or any other store based
-        //on your application.
-        res.writeHead(200, {
-            'content-type': 'text/plain'
-        });
-        res.write('received the data:\n\n');
-        res.end(util.inspect({
-            fields: fields,
-            files: files
-        }));
-    });
+ var form = new formidable.IncomingForm();
+ form.parse(req, function (err, fields, files) {
+  //Store the data from the fields in your data store.
+  //The data store could be a file or database or any other store based
+  //on your application.
+  res.writeHead(200, {
+   'content-type': 'text/plain'
+  });
+  res.write('received the data:\n\n');
+  res.end(util.inspect({
+   fields: fields,
+   files: files
+  }));
+ });
 }
 
 function processFormFieldsIndividual(req, res) {
-    //Store the data from the fields in your data store.
-    //The data store could be a file or database or any other store based
-    //on your application.
-    var fields = [];
-    var form = new formidable.IncomingForm();
-    form.on('field', function (field, value) {
-        console.log(field);
-        console.log(value);
-        fields[field] = value;
-    });
-
-    form.on('end', function () {
-        try {
-        sendMail(util.inspect(fields));
-        res.status(200)
-        res.write(process.env.MESSAGE || 'Thank you for your submission.');
-        res.end();
-        } catch(error) {
-        res.write('There are some error occurs! Please try again later');
-        res.end();
-        }
-    });
-    form.parse(req);
+ var fields = [];
+ var form = new formidable.IncomingForm();
+ form.on('field', function (field, value) {
+  console.log(field);
+  console.log(value);
+  fields[field] = value;
+ );
+ form.on('end', function () {
+  try {
+   sendMail(util.inspect(fields));
+   res.status(200)
+   res.write(process.env.MESSAGE || 'Thank you for your submission.');
+   res.end();
+  } catch(error) {
+   res.write('There are some error occurs! Please try again later');
+   res.end();
+  }
+  });
+ form.parse(req);
 }
 
 // setup the email sender
@@ -82,32 +79,31 @@ function processFormFieldsIndividual(req, res) {
 // sends the email to the adress found in the `TO` env var
 
 let transporter = nodemailer.createTransport({
-    service: process.env.SERVICE,
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_PORT === 465, // secure:true for port 465, secure:false for port 587
-    requireTLS: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+ service: process.env.SERVICE,
+ host: process.env.EMAIL_HOST,
+ port: process.env.EMAIL_PORT,
+ secure: process.env.EMAIL_PORT === 465, // secure:true for port 465, secure:false for port 587
+ requireTLS: true,
+ auth: {
+  user: process.env.EMAIL_USER,
+  pass: process.env.EMAIL_PASS
+ }
 });
 
 function sendMail(text) {
-  // setup email data with unicode symbols
-let mailOptions = {
-    from: process.env.FROM || 'Email form data bot <noreply.igorkowalczyk@gmail.com>',
-    to: process.env.TO,
-    subject: 'New form submission from' + (process.env.SITE_NAME ? ' on ' + process.env.SITE_NAME : ''),
-    text: text
+ // setup email data with unicode symbols
+ let mailOptions = {
+  from: process.env.FROM || 'Email form data bot <noreply.igorkowalczyk@gmail.com>',
+  to: process.env.TO,
+  subject: 'New form submission from' + (process.env.SITE_NAME ? ' on ' + process.env.SITE_NAME : ''),
+  text: text
 };
-  console.log('sending email: ', mailOptions);
-  // send mail with defined transport object
-  transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-          return console.log(error);
-      }
-      console.log('Message %s sent: %s', info.messageId, info.response);
-  });
+ console.log('sending email: ', mailOptions);
+ // send mail with defined transport object
+ transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+   return console.log(error);
+  }
+  console.log('Message %s sent: %s', info.messageId, info.response);
+ });
 }
-
